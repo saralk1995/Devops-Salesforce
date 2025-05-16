26a1bf65237d838a57c5c768a785de984f7583a9
saralk1995 <khandelwal.saral2008@gmail.com>
2025-05-16 13:59:29 +0930

    changes

--- scripts/generate-delta.sh
+++ scripts/generate-delta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Generating delta..."
 mkdir -p delta
-git fetch origin
-sgd delta --from origin/main --to HEAD --output delta
+git fetch origin main ST
+npx sfdx-git-delta delta --from origin/main --to HEAD --output delta
 echo "Delta contents"
 find delta
 echo "Delta generated in the 'delta' directory."
